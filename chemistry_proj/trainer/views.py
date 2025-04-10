9d7b068e360a72b2577edcde8e96633cc442619f
Николай Зелёный <nzeleny@yandex.ru>
2025-04-10 12:18:00 +0300

    Added main page
    Added main page and simple page with list of formulas from the database. Styles were added using bootstrap.

--- chemistry_proj/trainer/views.py
+++ chemistry_proj/trainer/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import ChemFormula
 
 def trainer(request):
-    return HttpResponse("Hello world!")
+	chemformulas = ChemFormula.objects.all().values()
+	template = loader.get_template('formulas_list.html')
+	context = {
+		'chemformulas': chemformulas,
+	}
+	return HttpResponse(template.render(context, request))
+
+def main(request):
+	template = loader.get_template('main.html')
+	return HttpResponse(template.render())
