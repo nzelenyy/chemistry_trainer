4602eb1c21ef77143fc32cb1bbadcbd1c6c854f4
Николай Зелёный <nzeleny@yandex.ru>
2025-04-13 19:38:24 +0300

    Added images removal from media folder

--- chemistry_proj/trainer/utils.py
+++ chemistry_proj/trainer/utils.py
@@ -1,6 +1,10 @@
+import os
+import re
+
 from .models import ChemFormula
 from .models import SubstanceImg
-import re
+from django.conf import settings
+
 
 def sub_format(text):
     return re.sub(r'(\d+)', r'<sub>\1</sub>', text)
@@ -14,8 +18,14 @@ def write_substance(name, substance):
     new_substance.save()
 
 def remove_substance(image_id):
-    image = SubstanceImg.objects.get(id=int(image_id))
-    image.delete()
+    substance = SubstanceImg.objects.get(id=int(image_id))
+
+    if substance.image:
+        file_path = os.path.join(settings.MEDIA_ROOT, str(substance.image))
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+    substance.delete()
 
 def remove_formula(formula_id):
     formula = ChemFormula.objects.get(id=int(formula_id))
